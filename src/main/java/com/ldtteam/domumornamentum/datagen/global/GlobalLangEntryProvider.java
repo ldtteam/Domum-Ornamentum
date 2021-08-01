9d8fd49913e8955aa1fe98ebd865f09e62bef2e0
Marc Hermans <marc.hermans@ldtteam.com>
2021-08-01 11:40:45 +0200

    ADd the CreateTabEntry header to the global lang data provider.

--- src/main/java/com/ldtteam/domumornamentum/datagen/global/GlobalLangEntryProvider.java
+++ src/main/java/com/ldtteam/domumornamentum/datagen/global/GlobalLangEntryProvider.java
@@ -36,6 +36,7 @@ public class GlobalLangEntryProvider implements DataProvider
         backingLangJson.put("itemGroup." + Constants.MOD_ID + ".fences", "DO - Fences");
         backingLangJson.put("itemGroup." + Constants.MOD_ID + ".slabs", "DO - Slabs");
         backingLangJson.put("itemGroup." + Constants.MOD_ID + ".walls", "DO - Walls");
+        backingLangJson.put("itemGroup." + Constants.MOD_ID + ".stairs", "DO - Stairs");
         backingLangJson.put("block." + Constants.MOD_ID + ".architectscutter", "Architects cutter");
         backingLangJson.put(Constants.MOD_ID + ".architectscutter", "Architects cutter");
 
