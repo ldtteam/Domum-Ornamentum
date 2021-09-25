58a4739a3efcc1b52bc5b337e812aaa61fb2a2d3
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-09-25 15:00:58 -0300

    Add barrel deco - hotfix

--- src/main/java/com/ldtteam/domumornamentum/datagen/global/GlobalRecipeProvider.java
+++ src/main/java/com/ldtteam/domumornamentum/datagen/global/GlobalRecipeProvider.java
@@ -7,6 +7,7 @@ import com.ldtteam.datagenerators.recipes.shaped.ShapedPatternJson;
 import com.ldtteam.datagenerators.recipes.shaped.ShapedRecipeJson;
 import com.ldtteam.domumornamentum.block.ArchitectsCutterBlock;
 import com.ldtteam.domumornamentum.block.ModBlocks;
+import com.ldtteam.domumornamentum.block.decorative.BarrelBlock;
 import com.ldtteam.domumornamentum.block.decorative.ExtraBlock;
 import com.ldtteam.domumornamentum.block.types.ExtraBlockType;
 import com.ldtteam.domumornamentum.util.DataGeneratorConstants;
@@ -48,6 +49,28 @@ public class GlobalRecipeProvider implements DataProvider
         final Path blockstatePath = recipeFolder.resolve(block.getRegistryName().getPath() + ".json");
 
         DataProvider.save(DataGeneratorConstants.GSON, cache, DataGeneratorConstants.serialize(json), blockstatePath);
+
+        final BarrelBlock standingBarrel = ModBlocks.getInstance().getStandingBarrel();
+        final ShapedPatternJson standingBarrelPattern =  new ShapedPatternJson("SWS","SWS","SWS");
+        final Map<String, RecipeIngredientKeyJson> standingBarrelKeys = new HashMap<>();
+        standingBarrelKeys.put("S", new RecipeIngredientKeyJson(new RecipeIngredientJson(Items.STICK.getRegistryName().toString(), false)));
+        standingBarrelKeys.put("W", new RecipeIngredientKeyJson(new RecipeIngredientJson(BlockTags.PLANKS.getName().toString(), true)));
+
+        final ShapedRecipeJson standingBarrelJson = new ShapedRecipeJson("global", standingBarrelPattern, standingBarrelKeys, new RecipeResultJson(1, standingBarrel.asItem().getRegistryName().toString()));
+        final Path standingBarrelPath = recipeFolder.resolve(standingBarrel.getRegistryName().getPath() + ".json");
+
+        DataProvider.save(DataGeneratorConstants.GSON, cache, DataGeneratorConstants.serialize(standingBarrelJson), standingBarrelPath);
+
+        final BarrelBlock layingBarrel = ModBlocks.getInstance().getLayingBarrel();
+        final ShapedPatternJson layingBarrelPattern =  new ShapedPatternJson("SSS","WWW","SSS");
+        final Map<String, RecipeIngredientKeyJson> layingBarrelKeys = new HashMap<>();
+        layingBarrelKeys.put("S", new RecipeIngredientKeyJson(new RecipeIngredientJson(Items.STICK.getRegistryName().toString(), false)));
+        layingBarrelKeys.put("W", new RecipeIngredientKeyJson(new RecipeIngredientJson(BlockTags.PLANKS.getName().toString(), true)));
+
+        final ShapedRecipeJson layingBarrelJson = new ShapedRecipeJson("global", layingBarrelPattern, layingBarrelKeys, new RecipeResultJson(1, layingBarrel.asItem().getRegistryName().toString()));
+        final Path layingBarrelPath = recipeFolder.resolve(layingBarrel.getRegistryName().getPath() + ".json");
+
+        DataProvider.save(DataGeneratorConstants.GSON, cache, DataGeneratorConstants.serialize(layingBarrelJson), layingBarrelPath);
     }
 
     @NotNull
