06ac3f73c6bfc2a4fcf875d292983b918785921b
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-03-03 13:55:39 -0300

    fix id

--- src/api/java/com/ldtteam/domumornamentum/recipe/ModRecipeTypes.java
+++ src/api/java/com/ldtteam/domumornamentum/recipe/ModRecipeTypes.java
@@ -1,6 +1,7 @@
 package com.ldtteam.domumornamentum.recipe;
 
 import com.ldtteam.domumornamentum.recipe.architectscutter.ArchitectsCutterRecipe;
+import com.ldtteam.domumornamentum.util.Constants;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
@@ -28,7 +29,7 @@ public class ModRecipeTypes
      */
     @SuppressWarnings("SameParameterValue")
     static <T extends Recipe<?>> RecipeType<T> register(String name) {
-        return Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(name), new RecipeType<T>() {
+        return Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(Constants.MOD_ID, name), new RecipeType<T>() {
             public String toString() {
                 return name;
             }
