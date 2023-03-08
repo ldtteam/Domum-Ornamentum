c6d1a944c5e6b4230906ccca6887014f4ba90661
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-03-08 15:02:24 +0100

    fix incorrect namings

--- src/main/java/com/ldtteam/domumornamentum/datagen/extra/ExtraLangEntryProvider.java
+++ src/main/java/com/ldtteam/domumornamentum/datagen/extra/ExtraLangEntryProvider.java
@@ -7,6 +7,22 @@ public class ExtraLangEntryProvider implements LanguageProvider.SubProvider
 {
     @Override
     public void addTranslations(LanguageProvider.LanguageAcceptor acceptor) {
-        acceptor.add(Constants.MOD_ID + ".extra.name.format", "%s %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format", "%s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.black", "Black %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.blue", "Blue %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.brown", "Brown %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.cyan", "Cyan %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.gray", "Gray %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.green", "Green %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.light_blue", "Light Blue %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.light_gray", "Light Gray %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.lime", "Lime %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.magenta", "Magenta %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.orange", "Orange %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.pink", "Pink %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.purple", "Purple %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.red", "Red %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.white", "White %s Extra");
+        acceptor.add(Constants.MOD_ID + ".extra.name.format.yellow", "Yellow %s Extra");
     }
 }
--- src/main/java/com/ldtteam/domumornamentum/datagen/floatingcarpet/FloatingCarpetLangEntryProvider.java
+++ src/main/java/com/ldtteam/domumornamentum/datagen/floatingcarpet/FloatingCarpetLangEntryProvider.java
@@ -3,15 +3,27 @@ package com.ldtteam.domumornamentum.datagen.floatingcarpet;
 import com.ldtteam.data.LanguageProvider;
 import com.ldtteam.domumornamentum.block.ModBlocks;
 import com.ldtteam.domumornamentum.block.decorative.FloatingCarpetBlock;
+import com.ldtteam.domumornamentum.util.Constants;
 
 public class FloatingCarpetLangEntryProvider implements LanguageProvider.SubProvider {
 
     @Override
     public void addTranslations(LanguageProvider.LanguageAcceptor acceptor) {
-        for (final FloatingCarpetBlock floatingCarpet : ModBlocks.getInstance().getFloatingCarpets())
-        {
-            acceptor.add(floatingCarpet.getDescriptionId(), floatingCarpet.getColor().getName().substring(0, 1).toUpperCase() +
-                    floatingCarpet.getColor().getName().substring(1) + " Floating carpet");
-        }
+        acceptor.add("block." + Constants.MOD_ID + ".black_floating_carpet", "Black Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".blue_floating_carpet", "Blue Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".brown_floating_carpet", "Brown Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".cyan_floating_carpet", "Cyan Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".gray_floating_carpet", "Gray Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".green_floating_carpet", "Green Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".light_blue_floating_carpet", "Light Blue Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".light_gray_floating_carpet", "Light Gray Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".lime_floating_carpet", "Lime Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".magenta_floating_carpet", "Magenta Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".orange_floating_carpet", "Orange Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".pink_floating_carpet", "Pink Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".purple_floating_carpet", "Purple Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".red_floating_carpet", "Red Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".white_floating_carpet", "White Floating Carpet");
+        acceptor.add("block." + Constants.MOD_ID + ".yellow_floating_carpet", "Yellow Floating Carpet");
     }
 }
