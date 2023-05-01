56e2a7ed27b505430b879b3ea8b3579fb2e19e22
Spumantii <rinehart_contracting@hotmail.com>
2023-05-01 01:06:33 -0700

    test 1

--- src/main/java/com/ldtteam/domumornamentum/block/AbstractPostBlock.java
+++ src/main/java/com/ldtteam/domumornamentum/block/AbstractPostBlock.java
@@ -1,7 +1,6 @@
 package com.ldtteam.domumornamentum.block;
 
 import com.ldtteam.domumornamentum.block.interfaces.IDOBlock;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
@@ -9,31 +8,27 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DirectionalBlock;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.level.block.state.properties.*;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import static net.minecraft.world.level.block.state.properties.BlockStateProperties.FACING;
+
 /**
  * The abstract class for structurize-added posts.
 
  */
-public abstract class AbstractPostBlock<B extends AbstractPostBlock<B>> extends DirectionalBlock implements IDOBlock<B>
+public abstract class AbstractPostBlock<B extends AbstractPostBlock<B>> extends HorizontalDirectionalBlock implements IDOBlock<B>
 {
 
     public static final EnumProperty<Half> HALF = BlockStateProperties.HALF;
+    public static final DirectionProperty    FACING = BlockStateProperties.FACING;
     public static final BooleanProperty    WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    /** collision objects
-    * */
+
     protected static final VoxelShape Y_AXIS_AABB = Block.box(6.0D, 0.0D, 6.0D, 10.D, 16.0D, 10.0D);
     protected static final VoxelShape Z_AXIS_AABB = Block.box(6.0D, 6.0D, 0.0D, 10.D, 10.0D, 16.0D);
     protected static final VoxelShape X_AXIS_AABB = Block.box(0.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D);
@@ -94,9 +89,10 @@ public abstract class AbstractPostBlock<B extends AbstractPostBlock<B>> extends 
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_57561_)
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
-        p_57561_.add(FACING, HALF, WATERLOGGED);
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING, HALF, WATERLOGGED);
     }
 
     @NotNull
