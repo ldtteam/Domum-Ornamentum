e7026af8cc4747e1dda5d307d57a87f12911ac73
Spumantii <rinehart_contracting@hotmail.com>
2023-05-11 00:23:56 -0700

    test 1

--- src/main/java/com/ldtteam/domumornamentum/block/AbstractBlockSlab.java
+++ src/main/java/com/ldtteam/domumornamentum/block/AbstractBlockSlab.java
@@ -1,18 +1,105 @@
 package com.ldtteam.domumornamentum.block;
 
 import com.ldtteam.domumornamentum.block.interfaces.IDOBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
+
+/**add the 4 new slab TYPEs to the ENUM
+ *
+ */
 public abstract class AbstractBlockSlab<B extends AbstractBlockSlab<B>> extends SlabBlock implements IDOBlock<B>
 {
+
+    public static final DirectionProperty    FACING = BlockStateProperties.FACING;
+    protected static final VoxelShape NORTH_AABB = Block.box(0.0D, 8.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape SOUTH_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+    protected static final VoxelShape EAST_AABB = Block.box(8.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape WEST_AABB = Block.box(0.0D, 0.0D, 0.0D, 8.0D, 16.0D, 16.0D);
     /**
      * Constructor of abstract class.
-     * @param properties the input properties.
+     *
      */
+    @NotNull
+    @Override
+    public VoxelShape getShape(BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context)
+    {
+        SlabType type = state.getValue(TYPE);
+        if (type == SlabType.DOUBLE) {
+            return Shapes.block();
+        }
+        if (type == SlabType.TOP) {
+            return TOP_AABB;
+        }
+        if (type == SlabType.BOTTOM) {
+            return BOTTOM_AABB;
+        }
+        switch (state.getValue(FACING)) {
+            case NORTH:
+                return NORTH_AABB;
+            case SOUTH:
+                return SOUTH_AABB;
+            case EAST:
+                return EAST_AABB;
+            case WEST:
+                return WEST_AABB;
+            case UP:
+                return TOP_AABB;
+            case DOWN:
+            default:
+                return BOTTOM_AABB;
+        }
+    }
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING);
+    }
     public AbstractBlockSlab(final Properties properties)
     {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.DOWN).setValue(TYPE, SlabType.BOTTOM).setValue(WATERLOGGED, Boolean.FALSE));
+
+    }
+    @Override
+    public BlockState getStateForPlacement(@NotNull final BlockPlaceContext context)
+    {
+        BlockState blockstate = this.defaultBlockState();
+        FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
+        Direction direction = context.getClickedFace();
+        BlockPos blockpos = context.getClickedPos();
+
+        /** if player is holding the shift key while clicking, set slab TYPE (N/E/W/S) based on FACING
+         * else use the base class method */
+
+        if (context.isSecondaryUseActive()) {
+            blockstate = blockstate.setValue(FACING, context.getHorizontalDirection().getOpposite());
+        }
+        if (blockstate.is(this)) {
+            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
+        }
+        else {
+            BlockState blockstate1 = this.defaultBlockState().setValue(TYPE, SlabType.BOTTOM).setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER));
+            return direction != Direction.DOWN && (direction == Direction.UP || !(context.getClickLocation().y - (double)blockpos.getY() > 0.5D)) ? blockstate1 : blockstate1.setValue(TYPE, SlabType.TOP);
+        }
     }
 
     @Override
--- src/main/java/com/ldtteam/domumornamentum/datagen/slab/SlabBlockStateProvider.java
+++ src/main/java/com/ldtteam/domumornamentum/datagen/slab/SlabBlockStateProvider.java
@@ -1,19 +1,23 @@
 package com.ldtteam.domumornamentum.datagen.slab;
 
 import com.ldtteam.domumornamentum.block.ModBlocks;
-import com.ldtteam.domumornamentum.block.vanilla.SlabBlock;
 import com.ldtteam.domumornamentum.datagen.MateriallyTexturedModelBuilder;
 import com.ldtteam.domumornamentum.datagen.utils.ModelBuilderUtils;
 import com.ldtteam.domumornamentum.util.Constants;
+import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import com.ldtteam.domumornamentum.block.vanilla.SlabBlock;
 
-import static net.minecraft.world.level.block.SlabBlock.TYPE;
+import static com.ldtteam.domumornamentum.block.vanilla.SlabBlock.TYPE;
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING;
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.SLAB_TYPE;
 
 public class SlabBlockStateProvider extends BlockStateProvider
 {
@@ -30,14 +34,20 @@ public class SlabBlockStateProvider extends BlockStateProvider
     private void createBlockstateFile(final SlabBlock slabBlock)
     {
         final MultiPartBlockStateBuilder builder = getMultipartBuilder(slabBlock);
-        for (SlabType value : SlabType.values()) {
-            builder.part()
-                    .modelFile(models().withExistingParent("block/slab/" + value.getSerializedName(), modLoc("block/slab/slab_" + value.getSerializedName() + "_spec"))
-                            .customLoader(MateriallyTexturedModelBuilder::new)
-                            .end())
-                    .addModel()
-                    .condition(TYPE, value)
-                    .end();
+        for (Direction facingValue : HORIZONTAL_FACING.getPossibleValues()) {
+            for (SlabType value : SlabBlock.TYPE.getPossibleValues()) {
+                builder.part()
+                        .modelFile(models().withExistingParent("block/slab/" + value.getSerializedName(), modLoc("block/slab/slab_" + value.getSerializedName() + "_spec"))
+                                .customLoader(MateriallyTexturedModelBuilder::new)
+                                .end())
+                        .rotationX(getXFromFacing(facingValue))
+                        .rotationY(getYFromFacing(facingValue))
+
+                        .addModel()
+                        .condition(HORIZONTAL_FACING, facingValue)
+                        .condition(SlabBlock.TYPE, value)
+                        .end();
+            }
         }
 
         final ItemModelBuilder itemModelBuilder = itemModels().withExistingParent(slabBlock.getRegistryName().getPath(), modLoc("item/slab/slab_spec"))
@@ -45,7 +55,26 @@ public class SlabBlockStateProvider extends BlockStateProvider
                 .end();
         ModelBuilderUtils.applyDefaultItemTransforms(itemModelBuilder);
     }
+    @Contract(pure = true)
+    private int getYFromFacing(final Direction facing) {
+        return switch (facing) {
+            default -> 0;
+            case SOUTH -> 180;
+            case WEST -> 270;
+            case EAST -> 90;
+        };
+    }
+    private int getXFromFacing(final Direction facing) {
+        return switch (facing) {
 
+            case UP -> 0;
+            case DOWN -> 180;
+            case NORTH -> 90;
+            case SOUTH -> 90;
+            case WEST -> 90;
+            case EAST -> 90;
+        };
+    }
     @NotNull
     @Override
     public String getName()
