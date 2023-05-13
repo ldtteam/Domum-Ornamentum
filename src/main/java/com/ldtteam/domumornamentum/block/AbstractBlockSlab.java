d9f25e4d6fd3c485a83441c5fe7882d1ebd6f07f
Spumantii <rinehart_contracting@hotmail.com>
2023-05-12 21:14:24 -0700

    test 1

--- src/main/java/com/ldtteam/domumornamentum/block/AbstractBlockSlab.java
+++ src/main/java/com/ldtteam/domumornamentum/block/AbstractBlockSlab.java
@@ -5,6 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
@@ -56,8 +57,13 @@ public abstract class AbstractBlockSlab<B extends AbstractBlockSlab<B>> extends 
                         return BOTTOM_AABB;
             }
         }
+        if (state.getValue(TYPE) == SlabType.TOP)   {
+                return TOP_AABB;
+        }
+        if (state.getValue(TYPE) == SlabType.BOTTOM)   {
+            return BOTTOM_AABB;
+        }
         return Shapes.block();
-
     }
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
@@ -74,64 +80,52 @@ public abstract class AbstractBlockSlab<B extends AbstractBlockSlab<B>> extends 
     @Override
     public BlockState getStateForPlacement(@NotNull final BlockPlaceContext context)
     {
-        BlockState blockstate = this.defaultBlockState();
+        BlockPos blockpos = context.getClickedPos();
+        BlockState defaultstate = this.defaultBlockState();
+        BlockState blockstate = context.getLevel().getBlockState(blockpos);
         FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
         Direction direction = context.getClickedFace();
-        BlockPos blockpos = context.getClickedPos();
 
+        /** stacking */
+        if (blockstate.is(this)) {
+            return defaultstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
+
+            /** TO DO only stack if clicking on the 'mid-way' face*/
+        }
 
         /** if player is holding the shift key while clicking, set slab TYPE (N/E/W/S) based on FACING
          * else use the base class method */
-
         if (context.isSecondaryUseActive()) {
-           return blockstate.setValue(FACING, context.getHorizontalDirection().getOpposite());
-            /**if (context.getLevel().getBlockState(blockpos).getValue(TYPE) == SlabType.TOP) {
-                return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-            }
-            if (context.getLevel().getBlockState(blockpos).getValue(TYPE) == SlabType.BOTTOM) {
-                return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-            }
-            if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.NORTH) {
-                return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-            }
-            if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.SOUTH) {
-                return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-            }
-            if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.EAST) {
-                return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-            }
-            if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.WEST) {
-                return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-            }*/
+           return defaultstate.setValue(FACING, context.getNearestLookingDirection().getOpposite());
+           /** TO DO I want to only do getOpposite on the horizontals.  Need condition here
+            *  I switched to getNearestLookingDirection so it would return up and down as it would not with gethorizontal */
         }
 
-
-        /** if there's an existing slab there make state double
-         * this is ugly, help me rewrite?
-         *
-        if (context.getLevel().getBlockState(blockpos).getValue(TYPE) == SlabType.TOP) {
-            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-        }
-        if (context.getLevel().getBlockState(blockpos).getValue(TYPE) == SlabType.BOTTOM) {
-            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-        }
-        if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.NORTH) {
-            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-        }
-        if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.SOUTH) {
-            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-        }
-        if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.EAST) {
-            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-        }
-        if (context.getLevel().getBlockState(blockpos).getValue(FACING) == Direction.WEST) {
-            return blockstate.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, Boolean.valueOf(false));
-        }
-        otherwise place as normal slabs do */
-        BlockState blockstate1 = this.defaultBlockState().setValue(TYPE, SlabType.BOTTOM).setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER));
-        return direction != Direction.DOWN && (direction == Direction.UP || !(context.getClickLocation().y - (double)blockpos.getY() > 0.5D)) ? blockstate1 : blockstate1.setValue(TYPE, SlabType.TOP);
+        /**default placing */
+        BlockState blockstate1 = this.defaultBlockState().setValue(TYPE, SlabType.BOTTOM).setValue(FACING, Direction.DOWN).setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER));
+        return direction != Direction.DOWN && (direction == Direction.UP || !(context.getClickLocation().y - (double)blockpos.getY() > 0.5D)) ? blockstate1 : blockstate1.setValue(FACING, Direction.UP);
     }
 
+    public boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
+        ItemStack itemstack = context.getItemInHand();
+        SlabType slabtype = state.getValue(TYPE);
+        if (slabtype != SlabType.DOUBLE && itemstack.is(this.asItem())) {
+            if (context.replacingClickedOnBlock()) {
+                /**did you click in the top half of the block*/
+                boolean flag = context.getClickLocation().y - (double)context.getClickedPos().getY() > 0.5D;
+                Direction direction = context.getClickedFace();
+                if (slabtype == SlabType.BOTTOM) {
+                    return direction == Direction.UP || flag && direction.getAxis().isHorizontal();
+                } else {
+                    return direction == Direction.DOWN || !flag && direction.getAxis().isHorizontal();
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
     @Override
     public ResourceLocation getRegistryName()
     {
