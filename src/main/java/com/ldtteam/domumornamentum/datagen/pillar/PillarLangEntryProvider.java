04d8597d45f5c9af9f2e5c64269b22b0622d4a73
someaddons <someaddons@github.com>
2025-09-22 23:14:08 +0200

    Reworked pillars to be more in minecraft style blocky now is a tougher square round now is a small square

--- src/main/java/com/ldtteam/domumornamentum/datagen/pillar/PillarLangEntryProvider.java
+++ src/main/java/com/ldtteam/domumornamentum/datagen/pillar/PillarLangEntryProvider.java
@@ -7,8 +7,8 @@ public class PillarLangEntryProvider implements LanguageProvider.SubProvider
 {
     @Override
     public void addTranslations(LanguageProvider.LanguageAcceptor acceptor) {
-        acceptor.add(Constants.MOD_ID + ".blockpillar.name.format", "Round %s Pillar");
-        acceptor.add(Constants.MOD_ID + ".blockypillar.name.format", "Voxel %s Pillar");
+        acceptor.add(Constants.MOD_ID + ".blockpillar.name.format", "Small Square %s Pillar");
+        acceptor.add(Constants.MOD_ID + ".blockypillar.name.format", "Tough Square %s Pillar");
         acceptor.add(Constants.MOD_ID + ".squarepillar.name.format", "Square %s Pillar");
 
         acceptor.add(Constants.MOD_ID + ".pillar.header", "Type:");
